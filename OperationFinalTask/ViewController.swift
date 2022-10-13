c93dd801938d91f2be6e00377d7d41c40592fe40
ru5s <hellperock@gmail.com>
2022-10-13 23:35:34 +0500

    Initial Commit

---
+++ OperationFinalTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OperationFinalTask
+//
+//  Created by Ruslan Ismailov on 13/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
